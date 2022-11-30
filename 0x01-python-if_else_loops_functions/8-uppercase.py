ea1d672690904a723e2b266ae6d9b5213f82cb9c
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2022-11-29 17:11:14 -0800

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    """Print a string in uppercase"""
+    for c in range(str):
+        if ord(c) >= 97 and ord(c) <= 123:
+            c = (chr(c) - 32)
+         print("{}".format(c), end="")
+        print("")
+
+
