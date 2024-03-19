6ede5faadd73cf51e6835c0c23670faa82a437e0
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2024-03-19 11:57:05 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     arr = dir()
     for i in range(0, len(arr)):
-        if arr[i][:2] != "_":
+        if arr[i][:2] != "__":
             print("{:s}".format(arr[i]))
