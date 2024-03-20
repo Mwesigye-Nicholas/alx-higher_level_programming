42b9c996a848449359f630d68f9b0f3ab2558fb0
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2024-03-20 09:21:54 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for i in range(len(my_list) - 1):
+    for i in range(len(my_list) -1):
         if new_list[i] == search:
             new_list[i] = replace
     return new_list
